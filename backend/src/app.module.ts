2652a90838c04b3322f3828f293b63309633b450
Rei Tsukada <rayzou1120@icloud.com>
2024-06-19 15:06:05 +0900

    Duplicate identifierの解消

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 
